121e740e2a70b12c9391b942712bfc1d9c86daf6
Kuan <n789142536@hotmail.com>
2022-03-10 10:16:33 +0800

    Initial Commit

---
+++ animateTest/animateTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  animateTestApp.swift
+//  animateTest
+//
+//  Created by 王冠之 on 2022/3/10.
+//
+
+import SwiftUI
+
+@main
+struct animateTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
